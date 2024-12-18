0642d46308e2f1666c28acee9d99a1a66c702c97
aymensellaouti <aymen.sellaouti@gmail.com>
2024-12-18 14:12:57 +0100

    correction exo ColorComponent

---
+++ src/app/components/color/color.component.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-color',
+  templateUrl: './color.component.html',
+  styleUrls: ['./color.component.css'],
+})
+export class ColorComponent {
+  /**
+   * La couleur par défaut de la div
+   */
+  defaultColor = 'green';
+  /**
+   * La couleur du background la div
+   */
+  color = this.defaultColor;
+
+  /**
+   * Changer la couleur du background
+   * @param newColor : La nouvelle couleur
+   */
+  changeColor(colorInput: HTMLInputElement) {
+    this.color = colorInput.value;
+    colorInput.value = '';
+  }
+
+  /**
+   * Remttre la couleur par défaut de la div
+   */
+  reset() {
+    this.color = this.defaultColor;
+  }
+}
