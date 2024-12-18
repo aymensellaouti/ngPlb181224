b4cdcbdf649b0635be2f104544a0b4bbccadeda5
aymensellaouti <aymen.sellaouti@gmail.com>
2024-12-18 14:29:13 +0100

    exemple 2way et standalone Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,19 +6,13 @@ import { AppComponent } from './app.component';
 import { FirstComponent } from './components/first/first.component';
 import { SecondComponent } from './components/second/second.component';
 import { ColorComponent } from './components/color/color.component';
+import { TwoComponent } from './components/two/two.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    FirstComponent,
-    SecondComponent,
-    ColorComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, FirstComponent, SecondComponent, ColorComponent],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, TwoComponent],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/components/two/two.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-two',
+  templateUrl: './two.component.html',
+  styleUrls: ['./two.component.css'],
+  standalone: true,
+  imports: [FormsModule]
+})
+export class TwoComponent {
+
+  two = 'initial value';
+}
