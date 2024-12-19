651c917bd1f6b5a65f10ab49d435d74d0e366828
aymensellaouti <aymen.sellaouti@gmail.com>
2024-12-19 12:13:09 +0100

    correction Todo Exercice

--- src/app/todo/todo/todo.component.ts
+++ src/app/todo/todo/todo.component.ts
@@ -1,4 +1,6 @@
-import { Component } from "@angular/core";
+import { Component, inject } from "@angular/core";
+import { Todo } from "../model/todo";
+import { TodoService } from "../service/todo.service";
 
 @Component({
   selector: 'app-todo',
@@ -7,5 +9,32 @@ import { Component } from "@angular/core";
 
 })
 export class TodoComponent {
-  constructor() {}
+
+  /**
+   * Le Todo qui représente notre formulaire
+   */
+  todo: Todo = new Todo();
+
+  /**
+   * La liste des todos
+   */
+  todos: Todo[] = [];
+
+  todoService = inject(TodoService);
+
+  constructor() {
+    this.todos = this.todoService.getTodos();
+  }
+
+  /**
+   * Permet d'ajouter un todo
+   */
+  addTodo() {
+    this.todoService.addTodo(this.todo);
+    this.todo = new Todo();
+  }
+
+  deleteTodo(todo: Todo) {
+    this.todoService.deleteTodo(todo);
+  }
 }
